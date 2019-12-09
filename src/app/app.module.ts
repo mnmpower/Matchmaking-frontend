2ea5d6205d3be169ddc301a2efde71f3315c3557
mnmpower <mnmmaarten@hotmail.com>
2019-12-09 22:06:00 +0100

    added flavers, started registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,14 @@ import { WelkomComponent } from './welkom/welkom.component';
 import { MakerModule } from './maker/maker.module';
 import { LoginComponent } from './login/login.component';
 import { BedrijfModule} from './bedrijf/bedrijf.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MaakopdrachtComponent} from './bedrijf/maakopdracht/maakopdracht.component';
+import { RegistreerComponent } from './registreer/registreer.component';
+import {BlockCopyPasteCutDirective} from './helpers/block-copy-paste-cut.directive';
+
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {SecurityInterceptor} from './security/security.interceptor';
+import {UserService} from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -19,16 +25,26 @@ import { MaakopdrachtComponent} from './bedrijf/maakopdracht/maakopdracht.compon
     FooterComponent,
     WelkomComponent,
     LoginComponent,
-    MaakopdrachtComponent
+    MaakopdrachtComponent,
+    RegistreerComponent,
+    BlockCopyPasteCutDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MakerModule,
     BedrijfModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: SecurityInterceptor,
+    multi: true,
+  },
+    UserService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { User } from '../models/user.model'
+import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -10,39 +10,43 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  //haal alle users op
+  // haal alle users op
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>("https://localhost:44316/api/user", {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.get<User[]>('https://localhost:44316/api/user', {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //haal user op voor id
+  // haal user op voor id
   getUser(id: number): Observable<User> {
-    return this.http.get<User>("https://localhost:44316/api/user/" + id, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.get<User>('https://localhost:44316/api/user/' + id, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //voeg een nieuw user toe
+  // voeg een nieuw user toe
   addUser(user: User) {
-    return this.http.post<User>("https://localhost:44316/api/user", user, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.post<User>('https://localhost:44316/api/user', user, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //wijzig user
+  // wijzig user
   updateUser(user: User) {
-    return this.http.put<User>("https://localhost:44316/api/user/" + user.userID, user, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.put<User>('https://localhost:44316/api/user/' + user.userID, user, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //verwijder user
+  // verwijder user
   deleteUser(id: number) {
-    return this.http.delete<User>("https://localhost:44316/api/user/" + id, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.delete<User>('https://localhost:44316/api/user/' + id, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
+  // check voor bruikbaar Mailadress
+  bestaatMailadres(mail: string) {
+    return this.http.get<boolean>('https://localhost:44316/api/user/bestaatMail/' + mail);
+  }
 }
