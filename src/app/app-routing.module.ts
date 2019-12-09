393a33d616b64bec2d8cd9d6c14c776124213056
BartOomsschool <r0445162@student.thomasmore.be>
2019-12-09 14:38:01 +0100

    logger aanmaken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelkomComponent} from './welkom/welkom.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   { path: '', component: WelkomComponent },
   { path: 'signUp', component: WelkomComponent },
-  { path: 'logIn', component: WelkomComponent },
+  { path: 'logIn', component: LoginComponent },
   // authenticatien toevoegen bij alle onderstaande!!!
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { WelkomComponent } from './welkom/welkom.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    WelkomComponent
+    WelkomComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
