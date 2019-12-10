08950d38a2fa9d8f59cb1faa8396061ebb364add
BartOomsschool <r0445162@student.thomasmore.be>
2019-12-10 14:52:11 +0100

    admin aanamken

--- src/app/admin/admin-overzicht/admin-overzicht.component.ts
+++ src/app/admin/admin-overzicht/admin-overzicht.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  templateUrl: './admin-overzicht.component.html',
+  styleUrls: ['./admin-overzicht.component.scss']
 })
 export class AdminComponent implements OnInit {
 
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminComponent } from './admin/admin.component';
+import { AdminComponent } from './admin-overzicht/admin-overzicht.component';
 
 
 
