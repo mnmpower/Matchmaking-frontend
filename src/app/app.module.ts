3be5013294fd404bf9ffb9be600a84bf827fa929
jintemichiels <r0701588@student.thomasmore.be>
2019-12-10 08:30:37 +0100

    Made home banner fullscreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { MakerModule } from './maker/maker.module';
 import { LoginComponent } from './login/login.component';
 import { BedrijfModule} from './bedrijf/bedrijf.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MaakopdrachtComponent} from './bedrijf/maakopdracht/maakopdracht.component';
 import { RegistreerComponent } from './registreer/registreer.component';
 import {BlockCopyPasteCutDirective} from './helpers/block-copy-paste-cut.directive';
 
@@ -25,7 +24,6 @@ import {UserService} from './services/user.service';
     FooterComponent,
     WelkomComponent,
     LoginComponent,
-    MaakopdrachtComponent,
     RegistreerComponent,
     BlockCopyPasteCutDirective
   ],
