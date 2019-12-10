cfc84b4e4d8809d6723dff07d3f25b96e4650baa
mnmpower <mnmmaarten@hotmail.com>
2019-12-10 14:00:10 +0100

    merched

--- src/app/services/bedrijf.service.ts
+++ src/app/services/bedrijf.service.ts
@@ -11,35 +11,35 @@ export class BedrijfService {
   constructor(private http: HttpClient) {
   }
 
-  //haal alle bedrijven op
+  // haal alle bedrijven op
   getBedrijven(): Observable<Bedrijf[]> {
     return this.http.get<Bedrijf[]>('https://localhost:44316/api/bedrijf', {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //haal bedrijf op voor id
+  // haal bedrijf op voor id
   getBedrijf(id: number): Observable<Bedrijf> {
     return this.http.get<Bedrijf>('https://localhost:44316/api/bedrijf/' + id, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //voeg een nieuw bedrijf toe
+  // voeg een nieuw bedrijf toe
   addBedrijf(bedrijf: Bedrijf) {
     return this.http.post<Bedrijf>('https://localhost:44316/api/bedrijf', bedrijf, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //wijzig bedrijf
+  // wijzig bedrijf
   updateBedrijf(bedrijf: Bedrijf) {
     return this.http.put<Bedrijf>('https://localhost:44316/api/bedrijf/' + bedrijf.bedrijfID, bedrijf, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-  //verwijder bedrijf
+  // verwijder bedrijf
   deleteBedrijf(id: number) {
     return this.http.delete<Bedrijf>('https://localhost:44316/api/bedrijf/' + id, {
       headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
