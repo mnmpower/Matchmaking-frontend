b2390a4dcecfb0632a765b88b3085623e59af76a
mnmpower <mnmmaarten@hotmail.com>
2019-12-10 13:46:33 +0100

    updates quotes voor services

--- src/app/services/opdracht.service.ts
+++ src/app/services/opdracht.service.ts
@@ -1,53 +1,54 @@
-import { Injectable } from '@angular/core';
-import { Opdracht } from '../models/opdracht.model';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { OpdrachtenFilter } from '../maker/filter/opdrachten-filter.model';
+import {Injectable} from '@angular/core';
+import {Opdracht} from '../models/opdracht.model';
+import {Observable} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {OpdrachtenFilter} from '../maker/filter/opdrachten-filter.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpdrachtService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   //haal alle opdrachten op
   getOpdrachten(): Observable<Opdracht[]> {
-    return this.http.get<Opdracht[]>("https://localhost:44316/api/opdracht", {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.get<Opdracht[]>('https://localhost:44316/api/opdracht', {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
-    //haal alle opdrachten op
-    getOpdrachtenFilter(filter: OpdrachtenFilter): Observable<Opdracht[]> {
-      return this.http.post<Opdracht[]>("https://localhost:44316/api/opdracht/filter", filter);
-    }
+  //haal alle opdrachten op
+  getOpdrachtenFilter(filter: OpdrachtenFilter): Observable<Opdracht[]> {
+    return this.http.post<Opdracht[]>('https://localhost:44316/api/opdracht/filter', filter);
+  }
 
   //haal opdracht op voor id
   getOpdracht(id: number): Observable<Opdracht> {
-    return this.http.get<Opdracht>("https://localhost:44316/api/opdracht/" + id, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.get<Opdracht>('https://localhost:44316/api/opdracht/' + id, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
   //voeg een nieuwe opdracht toe
   addOpdracht(opdracht: Opdracht) {
-    return this.http.post<Opdracht>("https://localhost:44316/api/opdracht", opdracht, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.post<Opdracht>('https://localhost:44316/api/opdracht', opdracht, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
   //wijzig opdracht
   updateOpdracht(opdracht: Opdracht) {
-    return this.http.put<Opdracht>("https://localhost:44316/api/opdracht/" + opdracht.opdrachtID, opdracht, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.put<Opdracht>('https://localhost:44316/api/opdracht/' + opdracht.opdrachtID, opdracht, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
   //verwijder opdracht
   deleteOpdracht(id: number) {
-    return this.http.delete<Opdracht>("https://localhost:44316/api/opdracht/" + id, {
-      headers: new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem("token"))
+    return this.http.delete<Opdracht>('https://localhost:44316/api/opdracht/' + id, {
+      headers: new HttpHeaders().set('Authorization', 'Bearer ' + localStorage.getItem('token'))
     });
   }
 
