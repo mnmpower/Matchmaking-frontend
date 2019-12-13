7032dbb03c714b9c953361c0d70715f977bd4586
mnmpower <mnmmaarten@hotmail.com>
2019-12-13 23:36:14 +0100

    navigate to dashboard after enw opdrahct

--- src/app/bedrijf/maakopdracht/maakopdracht.component.ts
+++ src/app/bedrijf/maakopdracht/maakopdracht.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 import {Router} from '@angular/router';
-import { OpdrachtService } from 'src/app/services/opdracht.service';
-import { BedrijfService } from 'src/app/services/bedrijf.service';
-import { UserService } from 'src/app/services/user.service';
+import {OpdrachtService} from 'src/app/services/opdracht.service';
+import {BedrijfService} from 'src/app/services/bedrijf.service';
+import {UserService} from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-maakopdracht',
@@ -27,12 +27,12 @@ export class MaakopdrachtComponent implements OnInit {
     private _userService: UserService
   ) {
     // Controleer of gebruiker permissie heeft om deze pagina te bekijken
-    this._userService.getPermissions().subscribe(result =>{
-      if(result.indexOf("ADD_OPDRACHT") == -1) {
+    this._userService.getPermissions().subscribe(result => {
+      if (result.indexOf('ADD_OPDRACHT') == -1) {
         this.router.navigate(['/forbidden']);
       }
     });
-    
+
     this.getBedrijf();
   }
 
@@ -45,32 +45,34 @@ export class MaakopdrachtComponent implements OnInit {
     });
   }
 
-  onSubmit(){
+  onSubmit() {
     this.submitted = true;
     this.CreateOpdrachtForm.addControl('bedrijfID', new FormControl(this.bedrijfID));
     this.CreateOpdrachtForm.addControl('competitie', new FormControl(this.competitie));
     console.log('gemaakte opdracht form: ', this.CreateOpdrachtForm.value);
     this._opdrachtService.addOpdracht(this.CreateOpdrachtForm.value).subscribe(result => {
-       console.log('gemaakte opdracht: ', result);
-       this.router.navigate(['/bedrijf/dashboard/', this.bedrijfID]);
+
+      console.log('gemaakte opdracht: ', result);
+      this.router.navigate(['/bedrijf/dashboard/', this.bedrijfID]);
+
     });
 
   }
 
-  getBedrijf(){
+  getBedrijf() {
     console.log(localStorage.getItem('token'));
-      // tslint:disable-next-line: align
-      this._bedrijfService.getBedrijfID().subscribe(result => {
-        this.bedrijfID = result;
-        console.log(result);
+    // tslint:disable-next-line: align
+    this._bedrijfService.getBedrijfID().subscribe(result => {
+      this.bedrijfID = result;
+      console.log(result);
 
-      });
+    });
   }
 
   setCompetitie() {
 
     this.competitie = !this.competitie;
-    console.log('competitie: ',  this.competitie);
+    console.log('competitie: ', this.competitie);
   }
 
 
